959a730cbc69fa3c4d8695a68f67ccacc0346960
Yuxin916 <caiyuxin001220@gmail.com>
2024-08-28 17:29:39 +0800

    finalize baseline bash

--- server_test_bash/baseline.bash
+++ server_test_bash/baseline.bash
@@ -10,15 +10,50 @@
 #PBS -o /home/i2r/stucaiy/scratch/yuxin_projects/icra2024/server_test_bash/baseline_multi_gpu.out
 #PBS -e /home/i2r/stucaiy/scratch/yuxin_projects/icra2024/server_test_bash/baseline_multi_gpu.error
 
-### single GPU
-#python habitat-baselines/habitat_baselines/run.py \
-#--config-name=objectnav/saved_config/baseline_one_episode.yaml \
-#habitat_baselines.num_environments=4 \
-#habitat_baselines.evaluate=True
+# Get the number of available GPUs
+num_gpus=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+# Define global variable
+LOG_DIR="baseline_debug"
 
-### Multi GPU
-set -x
-python -u -m torch.distributed.launch --nnodes=1 --nproc_per_node=3 \
---use_env \
-habitat-baselines/habitat_baselines/run.py \
---config-name=objectnav/saved_config/baseline_one_episode.yaml habitat_baselines.evaluate=False
+# Check if only one GPU is available
+if [ "$num_gpus" -eq 1 ]; then
+    echo "Running on Single GPU！！！！！！"
+    export HABITAT_ENV_DEBUG=0
+
+    python habitat-baselines/habitat_baselines/run.py \
+    --config-name=objectnav/saved_config/baseline_fix.yaml \
+    habitat_baselines.num_environments=4 \
+    habitat.dataset.train_eps=1 \
+    habitat_baselines.num_environments=6 \
+    habitat_baselines.trainer_name=ddppo \
+    habitat_baselines.log_interval=5 \
+    habitat_baselines.rl.ppo.num_steps=128 \
+    habitat_baselines.rl.ppo.num_steps=64 \
+    habitat_baselines.tensorboard_dir="log/tb/${LOG_DIR}" \
+    habitat_baselines.video_dir="log/video_dir/${LOG_DIR}" \
+    habitat_baselines.eval_ckpt_path_dir="ckpt/${LOG_DIR}/latest.pth" \
+    habitat_baselines.checkpoint_folder="ckpt/${LOG_DIR}/" \
+    habitat_baselines.log_file="log/log/${LOG_DIR}.log" \
+    habitat_baselines.evaluate=False
+else
+    echo "Running on $num_gpus GPUs"
+    export HABITAT_ENV_DEBUG=0
+
+    set -x
+    python -u -m torch.distributed.launch --nnodes=1 --nproc_per_node=$num_gpus \
+    --use_env \
+    habitat-baselines/habitat_baselines/run.py \
+    --config-name=objectnav/saved_config/baseline_fix.yaml \
+    habitat.dataset.train_eps=1 \
+    habitat_baselines.num_environments=6 \
+    habitat_baselines.trainer_name=ddppo \
+    habitat_baselines.log_interval=5 \
+    habitat_baselines.rl.ppo.num_steps=128 \
+    habitat_baselines.rl.ppo.num_steps=64 \
+    habitat_baselines.tensorboard_dir="log/tb/${LOG_DIR}" \
+    habitat_baselines.video_dir="log/video_dir/${LOG_DIR}" \
+    habitat_baselines.eval_ckpt_path_dir="ckpt/${LOG_DIR}/latest.pth" \
+    habitat_baselines.checkpoint_folder="ckpt/${LOG_DIR}/" \
+    habitat_baselines.log_file="log/log/${LOG_DIR}.log" \
+    habitat_baselines.evaluate=False
+fi
